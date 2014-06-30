ae5992a4ae1c76dee61e588e63243fe4f2b951ca
aviillouz <avi.illouz@gmail.com>
2014-06-30 14:54:33 +0300

    Initial Commit of 2nd shot

--- src/ClassTwo.java
+++ src/ClassTwo.java
@@ -1,6 +1,5 @@
 /**
- * Created by Avi on 30/06/14.
- * Trying to commit only src files
+ * Trying to commit only src files.
  */
 public class ClassTwo {
     public static void main(String args[]){
